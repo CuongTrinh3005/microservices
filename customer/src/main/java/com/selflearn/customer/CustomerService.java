7cc12738df4e587a9670c3d86828a6ddfd9270bf
Cuong Trinh <cuongtrinhq@gmail.com>
2023-10-12 16:54:44 +0700

    add rest-template for customer module

--- customer/src/main/java/com/selflearn/customer/CustomerService.java
+++ customer/src/main/java/com/selflearn/customer/CustomerService.java
@@ -2,6 +2,7 @@ package com.selflearn.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -9,6 +10,8 @@ import java.util.List;
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public List<Customer> getAll(){
         return customerRepository.findAll();
     }
@@ -22,6 +25,17 @@ public class CustomerService {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        // todo: check if fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        assert fraudCheckResponse != null;
+        if(fraudCheckResponse.isFraudster())
+            throw new IllegalStateException("fraudster");
+        // todo: send notification
     }
 }
