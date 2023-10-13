20208dad8b90a2909a04e552d972444174e31bc7
Cuong Trinh <cuongtrinhq@gmail.com>
2023-10-13 16:21:39 +0700

    add load balancer in customer service rest template

--- customer/src/main/java/com/selflearn/customer/CustomerConfig.java
+++ customer/src/main/java/com/selflearn/customer/CustomerConfig.java
@@ -1,5 +1,6 @@
 package com.selflearn.customer;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class CustomerConfig {
     @Bean
+    @LoadBalanced // round-robin load balancer algorithm by default
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
